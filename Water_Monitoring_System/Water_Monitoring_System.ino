21e2301111b4a8bbc47338139bd9a2ec09d869fc
KMuriungi <kamkith@gmail.com>
2019-07-01 09:08:42 -0700

    Add files via upload

---
+++ Water_Monitoring_System/Water_Monitoring_System.ino
@@ -0,0 +1,201 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+const int buttonPinSelect = 51;    // the number of the pushbutton pin
+const int buttonPinReset = 53;
+
+int redPin = 23;      // the number of the LED pin
+int greenPin =25;
+int bluePin =27;
+
+int buttonSelCounter = 0;   // counter for the number of button presses
+int buttonSelState = 0;         // current state of the button
+int lastButtonSelState = 0;     // previous state of the button
+
+int buttonResCounter = 0;   // counter for the number of button presses
+int buttonResState = 0;         // current state of the button
+int lastButtonResState = 0;     // previous state of the button
+
+#define SENSOR       A7 //Turbidity Sensor 
+float voltage, turbidity, finalturbidity, phValue;
+
+#define SensorPin A0          //pH meter Analog output to Arduino Analog Input 0
+unsigned long int avgValue;  //Store the average value of the sensor feedback
+float b;
+int buf[10],temp;
+
+void setup() {
+  pinMode(buttonPinSelect, INPUT);
+  pinMode(buttonPinReset, INPUT);
+  Serial.begin(9600);
+  
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  pinMode(SENSOR,INPUT); //Turbidity
+
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("   MAJI SAFI   ");
+  lcd.setCursor(0,1);
+  lcd.print("*Health Living*");
+  delay(5000);
+}
+
+ /*
+ ************************************************************
+ *----------------- MAIN FUNCTION -------------------*
+ ************************************************************
+*/
+
+void loop() {
+ 
+/*
+STATE SELECT BUTTON
+*/
+
+  buttonSelState = digitalRead(buttonPinSelect);
+  if (buttonSelState != lastButtonSelState) {
+    if (buttonSelState == HIGH) {
+      buttonSelCounter++;
+      Serial.println("on");
+      Serial.print("number of button pushes:  ");
+      Serial.println(buttonSelCounter);
+    } else {
+      Serial.println("off");
+    }
+    delay(50);
+  }
+  lastButtonSelState = buttonSelState;
+  if (buttonSelCounter % 2 == 0) {
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(bluePin, LOW);
+    turbid();
+  } else if(buttonSelCounter % 3 == 0) {
+    digitalWrite(bluePin, HIGH);
+    digitalWrite(greenPin, LOW);
+    pHSensor();
+  }else {
+    digitalWrite(bluePin, HIGH);
+    digitalWrite(greenPin, LOW);
+
+    if(finalturbidity < 1.4 && phValue > 6.2 && phValue < 7.8){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("   MAJI SAFI   ");
+  lcd.setCursor(0, 1);
+  lcd.print("Fit for Drinking");
+  }else{
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("  MAJI CHAFU   ");
+    lcd.setCursor(0, 1);
+    lcd.print("Not Fit 4 Drink");
+    }
+  }
+  
+ /*
+ RESET BUTTON
+ */
+ 
+  buttonResState = digitalRead(buttonPinReset);
+  if (buttonResState != lastButtonResState) {
+    if (buttonResState == LOW) {
+      buttonResCounter++;
+      Serial.println("on");
+      Serial.print("number of button pushes:  ");
+      Serial.println(buttonResCounter);
+    } else {
+      Serial.println("off");
+    }
+    delay(50);
+  }
+  lastButtonResState = buttonResState;
+  if (buttonResCounter % 2 == 0) {
+    // NOTHING HERE
+    //Addition of Anything Affects the Normal Working of the Entire System
+    //Leave it as it is!!! 
+  } else {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, LOW);
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("   MAJI SAFI   ");
+    lcd.setCursor(0,1);
+    lcd.print("*Health Living*");
+  }
+
+} //End Loop()
+
+/*
+ ************************************************************
+ *                                                          *
+ **********************CUSTOM FUNCTIONS**********************
+ *                                                          *
+ ************************************************************
+*/
+
+  /*
+ ************************************************************
+ *----------------- TURBIDITY (PHYSICAL) -------------------*
+ ************************************************************
+*/
+
+void turbid(){
+  voltage=0.004888*analogRead(SENSOR);  //in V
+  turbidity=-1120.4*voltage*voltage+5742.3*voltage-4352.9;  //in NTU fron 0-5000
+  finalturbidity = (turbidity/1000); // in NTU from 0-5
+  if((voltage>=2.5)&(turbidity>=0))
+  {
+    Serial.println(" Turbidity = "+String(turbidity)+" NTU");  
+    delay(500);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("   MAJI SAFI   ");
+    lcd.setCursor(2, 1);
+    lcd.print("Turbid: "+String(finalturbidity));
+  }
+}
+
+/*
+ ************************************************************
+ *----------------- pH Sensor (CHEMICAL)  --------------------*
+ ************************************************************
+*/
+void pHSensor(){
+  for(int i=0;i<10;i++)       //Get 10 sample value from the sensor for smooth the value
+  { 
+    buf[i]=analogRead(SensorPin);
+    delay(10);
+  }
+  for(int i=0;i<9;i++)        //sort the analog from small to large
+  {
+    for(int j=i+1;j<10;j++)
+    {
+      if(buf[i]>buf[j])
+      {
+        temp=buf[i];
+        buf[i]=buf[j];
+        buf[j]=temp;
+      }
+    }
+  }
+  avgValue=0;
+  for(int i=2;i<8;i++)                      //take the average value of 6 center sample
+    avgValue+=buf[i];
+  phValue=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
+  phValue=2.90*phValue;                      //convert the millivolt into pH value
+  Serial.print("    pH:");  
+  Serial.print(phValue,2);
+  Serial.println(" ");
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("   MAJI SAFI   ");
+  lcd.setCursor(3, 1);
+  lcd.print("pH: "+String(phValue));
+  }
+
---
+++ waterCode2/waterCode2.ino
@@ -0,0 +1,137 @@
+
+//Turbidity = 100 - (Vout/Vclear)*100
+
+#include <EEPROM.h> // to store last calibration value (blank, Vclear)
+#include <Wire.h>  // for LCD display (with I2S interphase)
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+int sensor = 0; // variable for averaging
+int n = 10; // number of samples to average 
+int sensorValue = 0;
+float voltage = 0.00;
+float turbidity = 0.00;
+float Vclear = 2.85; // Output voltage to calibrate (with clear water).
+
+float satSolids = 0.00;
+
+int readingButton = 6;  
+int resetButton = 7;  
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(resetButton, INPUT_PULLUP); //initializes digital pin 7 as an input with pull-up resistance.
+  pinMode(readingButton, INPUT);
+ 
+ // LCD display
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("** Turbidity **");
+  lcd.setCursor(1,3);
+  lcd.print("*** Meter ***");
+
+//Serial display
+  Serial.println("Hi... welcome to your turbidity sensor");
+  
+  EEPROM.get(0, Vclear);  // recovers the last Vclear calibration value stored in ROM.
+  delay(5000);  // Pause for 3 sec
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Turbidity = ");
+  lcd.print(turbidity,2);
+  Serial.print(turbidity,2);
+  lcd.setCursor(0,1);
+  lcd.print("Volts = ");
+  lcd.print(voltage,2);
+ // lcd.print("Solids = ");
+  //lcd.print(satSolids,2);
+  Serial.print(satSolids,2);
+  
+}
+
+void loop()
+{
+readingButton = digitalRead(6);  // push button status
+
+if (readingButton == HIGH) {
+// If the push button is not pushed, do the normal sensing routine:
+      for (int i=0; i < n; i++){
+      sensor += analogRead(A0); // read the input on analog pin 0 (turbidity sensor analog output)      
+  delay(10);
+      }       
+  sensorValue = sensor / n;    // average the n values  
+  voltage = sensorValue * (5.000 / 1023.000); // Convert analog (0-1023) to voltage (0 - 5V)
+
+  satSolids = ((1.034 * (voltage)) + 38.1);
+  
+  turbidity = 100.00 - (voltage / Vclear) * 100.00; // as relative percentage; 0% = clear water; 
+  
+  EEPROM.put(0, Vclear); // guarda el voltaje de calibración actualmente en uso.
+          
+          // Serial display
+          Serial.print(sensorValue);
+          Serial.print(", ");
+          Serial.print(voltage,3);
+          Serial.print(", ");
+          Serial.print(satSolids,3);
+          Serial.print(", ");
+          Serial.print(turbidity,3);
+          Serial.print(", ");
+          Serial.println(Vclear,3);
+   
+  // Display LCD
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Turbidity = ");
+  lcd.print(turbidity,2);
+  Serial.print(turbidity,2);
+  lcd.setCursor(0,1);
+  lcd.print("Solids = ");
+  lcd.print(satSolids,2);
+  Serial.print(satSolids,2);
+
+  delay(6000);
+
+  sensor = 0;   // resets for averaging
+    
+  } else {
+
+// Calibration routine, when push button is pushed:
+  
+  Serial.println("Put the sensor in clear water to calibrate...");
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Calibrating");
+  lcd.setCursor(11,0);
+  lcd.print(".");
+  delay(200);
+  lcd.setCursor(12,0);
+  lcd.print(".");
+  delay(200);
+  lcd.setCursor(13,0);
+  lcd.print(".");
+  delay(200);
+  lcd.setCursor(14,0);
+  lcd.print(".");
+  delay(200);
+  lcd.setCursor(15,0);
+  lcd.print(".");
+  delay(2000); 
+  
+      for (int i=0; i < n; i++){
+      sensor += analogRead(A0); // read the input on analog pin 1:
+      delay(10);
+      }
+  sensorValue = sensor / n;
+  Vclear = sensorValue * (5.000 / 1023.000); // Converts analog (0-1023) to voltage (0-5V):
+  sensor = 0;
+  EEPROM.put(0, Vclear);  // stores Vclear in ROM
+  delay(1000);
+  lcd.clear();  
+  }  
+  delay(1000);  // Pause for 1 seconds.  // sampling rate
+}
